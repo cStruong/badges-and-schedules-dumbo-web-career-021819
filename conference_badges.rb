10106197d58ec00575b7da25c5c3ac2c796e6301
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-26 04:25:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(speakers)
   roomsArr = []
   
-  speakers.each {|speaker, index| roomsArr << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+  speakers.each_with_index {|speaker, index| roomsArr << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
   
   
   return roomsArr
